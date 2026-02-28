2e672eae4015585c0f08c23f2725d93cf52a7882
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-02-28 14:50:13 -0500

    fix: added missing env variable definition

--- packages/core/prisma.config.ts
+++ packages/core/prisma.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, env } from '@prisma/config';
+import 'dotenv/config';
 
 import type { PrismaConfig } from 'prisma';
+
+const POSTGRES_URL = process.env.POSTGRES_URL || 'http://localhost:3000';
 
 export default defineConfig({
     schema: 'prisma/schema',
